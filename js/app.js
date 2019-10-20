26973c16a30ed343603058a21e3297cedb53c43b
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2019-10-19 23:17:36 -0300

    Upload Full Libraries Assest and Create custom Styles and Javascript

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('body').scrollspy({  offset: 0, target: '#navbar-example2' })
+
+    // EmailJS
+    
+    // emailjs.init("user_UtSNY8gAuJymYvlvOJ22I");
+    
+    // var form = document.forms[0].elements;
+    
+    // form["submit"].addEventListener("click", function(){
+
+    //     var template_params = {
+    //         "from_name":form["from"] ,
+    //         "to_name": form["to"],
+    //         "message_html": form["massage"] 
+    //     };
+
+    //     var service_id = "default_service";
+    //     var template_id = "template_5oB5q5yN";
+    //     //emailjs.send(service_id, template_id, template_params);
+    //  });
+    
+
+
+});
